f9c4bad19780a8da64afc65dc0d9cb5565854468
Sea Stone <haithach.01202@gmail.com>
2026-05-14 14:24:51 +0700

    feat: Update global exception filter

--- src/core/common/exceptions/global-exception.filter.ts
+++ src/core/common/exceptions/global-exception.filter.ts
@@ -1,28 +1,48 @@
 import {
   Catch,
-  HttpException,
   ExceptionFilter,
   ArgumentsHost,
+  HttpException,
+  HttpStatus,
 } from '@nestjs/common';
-import { Request, Response } from 'express';
 
-@Catch(HttpException)
+import { Request, Response } from 'express';
+import { ERROR_CODES } from '../errors/error-code';
+
+@Catch()
 export class GlobalExceptionFilter implements ExceptionFilter {
-  catch(exception: HttpException, host: ArgumentsHost) {
+  catch(exception: unknown, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
 
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
 
-    const status = exception.getStatus();
+    const isHttpException = exception instanceof HttpException;
 
-    const exceptionResponse = exception.getResponse() as any;
+    const status = isHttpException
+      ? exception.getStatus()
+      : HttpStatus.INTERNAL_SERVER_ERROR;
 
-    const errorCode = exceptionResponse?.errorCode;
+    const exceptionResponse = isHttpException ? exception.getResponse() : null;
+
+    let errorCode =
+      typeof exceptionResponse === 'object'
+        ? (exceptionResponse as any)?.errorCode
+        : null;
+
+    let message = ERROR_CODES.INTERNAL_SERVER_ERROR.INTERNAL_SERVER_ERROR; // Default error code
+
+    if (isHttpException) {
+      if (typeof exceptionResponse === 'object') {
+        message = (exceptionResponse as any)?.message || exception.message;
+      } else {
+        message = exception.message;
+      }
+    }
 
     response.status(status).json({
       statusCode: status,
-      message: errorCode || exception.message,
+      message,
       errorCode,
       timestamp: new Date().toISOString(),
       path: request.url,
